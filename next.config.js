3f09ab09e151cb49839f9201b73b4dadfd15f4df
AditiDhumal2 <167055939+AditiDhumal2@users.noreply.github.com>
2025-11-27 16:08:49 +0530

    fix: simple config for vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,35 +1,7 @@
-﻿ /** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Build optimizations
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  
-  // MongoDB package externalization
-  serverExternalPackages: ['mongoose'],
-  
-  // Webpack configuration for WebSocket dependencies
-  webpack: (config) => {
-    config.externals.push(
-      { 'utf-8-validate': 'commonjs utf-8-validate' },
-      { 'bufferutil': 'commonjs bufferutil' }
-    )
-    return config
-  },
-  
-  // Image optimization
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http', 
-        hostname: 'localhost',
-        port: '3000',
-      },
-    ],
-  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 }
-
 module.exports = nextConfig
